3c17a4e98a56bbb22653e43ef1f793ccc146cabd
Tomasz <loobrome@gmail.com>
2021-12-09 18:13:39 +0100

    Adding new functions

--- OtoMotoProgramClass.py
+++ OtoMotoProgramClass.py
@@ -8,66 +8,52 @@ class OtoMotoProgram:
     def __init__(self):
         load_dotenv()
         self.URL = os.environ.get("URL")
-        self.response = requests.get(self.URL)
-        self.soup = bs.BeautifulSoup(self.response.content, 'html5lib')
-        self.soupsecond = bs.BeautifulSoup(self.response.content, 'html.parser')
+        self.response = requests.get(self.URL).text
+        self.soup = bs.BeautifulSoup(self.response, 'html5lib')
         self.cars_list = []
+        self.page_list = []
 
     def Start(self):
         print(self.URL)
         self.DownloadPage()
-        # print(self.soupsecond.text)
+        self.Work_with_data()
 
     def DownloadPage(self):
-        # cars_price = self.soup.find_all('span', class_='offer-price__number ds-price-number')
-
-
-        # cars_price = self.soup.find('span', class_='offer-price__number ds-price-number').contents[1]
-        # for price in cars_price:
-        #     self.cars_list.append(price)
-        #     print(price)
-        #
-        # print(self.cars_list)
-
-        # cars_price = self.soup.find_all('span', class_='offer-price__number ds-price-number')
-        # for price in cars_price:
-        #     self.cars_list.append(price.contents[1])
-        #     print(price)
-        #
-        # print(len(cars_price) ,"\n", cars_price[0])
-
-        #Good way to find text in spans
-        # cars_price = self.soup.find_all('span', class_='offer-price__number ds-price-number')
-        #
-        # for price in cars_price:
-        #     price = price.find_next('span')
-        #     self.cars_list.append(price.text)
-        #     # print(price.string)
-
-        def Find_Span():
+        def find_price():
+            """Finding price of cars in page"""
             cars_price = self.soup.find_all('span', class_='offer-price__number ds-price-number')
 
             for price in cars_price:
                 price = price.find_next('span')
                 self.cars_list.append(price.text)
 
-            webpageslist = []
+        def find_page(func):
+            """Downloads amount of pages"""
+            webpages = self.soup.findAll('span', class_="page")
+            self.page_list = []
+            for page in webpages:
+                self.page_list.append(page.text)
 
-            # webpages = self.soup.find_all("a", class_='optimus-app-g4wbjr ekxs86z0')
-            # webpages = self.soup.find_all("ul", class_='pagination-list  optimus-app-1tibvxz')
+            return func
 
-            #TODO Get number from span to get count of pages
-            webpages = self.soupsecond.find('div', class_='optimus-app-1oll9pn e19uumca7')
-            print(webpages)
-            print(webpageslist)
-            # for page in webpages:
-            #     # page = page.find_next('span')
-            #     webpageslist.append(page)
-            #     # print(page)
-            #TODO Go to this pages and download data
+        @find_page
+        def go_to_page():
+            """Going to all pages and downloads data to list"""
+            for page in range(1, int(self.page_list[-1])):
+                find_price()
 
-            #TODO Sort Data in self.cars_list (There is a lot of mess data)
-            # print(webpages)
+        go_to_page()
 
-        Find_Span()
+    def Work_with_data(self):
+        def sort_cars_list():
+            """Sorting elements in cars list, removing 'od' and 'price'
+            cause its advertising elements"""
+            for element in self.cars_list:
+                if element == "od":
+                    index = self.cars_list.index("od")
+                    del self.cars_list[index:index+2]
+                else:
+                    continue
+
+        sort_cars_list()
         print(self.cars_list)
