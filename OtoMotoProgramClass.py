d923690ebddcd47818f762d472900a80a83ca555
Tomasz <loobrome@gmail.com>
2021-12-23 18:58:45 +0100

    Fixing problem with wrong URL-price to Price in dict

--- OtoMotoProgramClass.py
+++ OtoMotoProgramClass.py
@@ -9,47 +9,48 @@ class OtoMotoProgram:
         load_dotenv()
         self.URL = os.environ.get("URL")
         self.response = requests.get(self.URL).text
-        self.soup = bs.BeautifulSoup(self.response, 'html5lib')
+        # self.soup = bs.BeautifulSoup(self.response, 'html5lib')
+        self.soup = bs.BeautifulSoup(self.response, 'html.parser')
         self.price_cars_list = []
         self.link_cars_list = []
         self.page_list = []
         self.index_list = []
+        self.car_dict = {}
 
     def Start(self):
-        print(self.URL)
+        # print(self.URL)
         self.DownloadPage()
-        # self.Work_with_data()
+        self.Work_with_data()
+
 
     def DownloadPage(self):
 
         def find_price():
             """Finding price of cars in page"""
-            cars_price = self.soup.findAll('span', class_='offer-price__number ds-price-number')
+            # cars_price = self.soup.findAll('span', class_='offer-price__number ds-price-number')
+            cars_price = self.soup.findAll('span', class_='optimus-app-epvm6 e1b25f6f8')
+
             for price in cars_price:
-                # price = price.find_next('span')
-                price = price.find('span')
-                self.price_cars_list.append(price.text)
+                # self.price_cars_list.append(price.text)
+                self.price_cars_list.append(str(price.text).strip('PLN'))
 
         def find_link():
-            cars_links = self.soup.findAll('div', class_='offer-item__title')
-            index = 0
+            """Finding link of car"""
+            # cars_links = self.soup.findAll('div', class_='offer-item__title')
+            cars_links = self.soup.findAll('h2',
+                                           class_='e1b25f6f13 optimus-app-1mgjl0z-Text eu5v0x0')
+
             for link in cars_links:
                 # link = link.find('h2', class_='offer-title ds-title').text
-                link = link.find('a', class_='offer-title__link', href=True)
+                # link = link.find('a', class_='offer-title__link', href=True)
+                link = link.find('a', href=True)
                 self.link_cars_list.append(link['href'])
-                # key = 'otomotoklik.pl'
-                # if key not in link['href']:
-                #     self.link_cars_list.append(link['href'])
-                #     index += 1
-                # else:
-                #     self.index_list.append(index)
-                #     index += 1
-                #     continue
 
         def find_page(func):
             """Downloads amount of pages"""
-            webpages = self.soup.findAll('span', class_="page")
-            print(webpages)
+            # webpages = self.soup.findAll('span', class_="page")
+            webpages = self.soup.findAll('a', class_='optimus-app-g4wbjr ekxs86z0')
+            # print(webpages)
             self.page_list = []
             for page in webpages:
                 self.page_list.append(page.text)
@@ -58,7 +59,7 @@ class OtoMotoProgram:
         @find_page
         def go_to_page():
             """Going to all pages and downloads data to list"""
-            print(self.page_list)
+            # print(self.page_list)
             pages = int(self.page_list[-1]) + 1
             self.URL = (self.URL[:] + f"&page=0")
             for page in range(pages):
@@ -67,49 +68,56 @@ class OtoMotoProgram:
                 find_link()
                 find_price()
                 self.URL = (self.URL[:-1] + f"{page}")
-                print(self.URL)
-
+                print(f'Wczytuję {self.URL}')
 
         go_to_page()
-        # print(f'indexes of otomotoklik is: {self.index_list}')
-        # print(self.response)
+
     def Work_with_data(self):
 
-        def sort_cars_list():
-            """Sorting elements in cars list, removing 'od' and 'price'
-            cause its advertising elements"""
-            # for element in self.price_cars_list:
-            #     if element == "od":
-            #         index = self.price_cars_list.index("od")
-            #         del self.price_cars_list[index:index + 2]
-            #
-            #     else:
-            #         continue
-            for element in self.price_cars_list:
-                if element == "od":
-                    index = self.price_cars_list.index("od")
-                    # del self.price_cars_list[index: index + 1]
-                    del self.price_cars_list[index]
+        # def sort_cars_list():
+        #     """Sorting elements in cars list, removing 'od' and 'price'
+        #     cause its advertising elements"""
+        #
+        #     # for element in self.price_cars_list:
+        #     #     if element == "od":
+        #     #         index = self.price_cars_list.index("od")
+        #     #         del self.price_cars_list[index:index + 2]
+        #     #
+        #     #     else:
+        #     #         continue
+        #     for element in self.price_cars_list:
+        #         if element == "od":
+        #             index = self.price_cars_list.index("od")
+        #             # del self.price_cars_list[index: index + 1]
+        #             del self.price_cars_list[index]
+        #
+        #         else:
+        #             continue
+        #
+        # def sort_price_list():
+        #     for element in self.index_list:
+        #         self.price_cars_list.pop(element)
 
-                else:
-                    continue
+        #TODO Sortuj dictionary poprzez klucz, dokończ funkcję asceding()
 
-        def sort_price_list():
-            for element in self.index_list:
-                self.price_cars_list.pop(element)
+        # def price_ascending():
+            # for key in self.car_dict.keys():
+            #     print(key)
+            # for key in sorted(self.car_dict.keys()):
+            #     print(key)
+            # self.car_dict = {k: v for k, v in sorted(list(self.car_dict.items()))}
+
+        def create_label():
+            zip_iterator = zip(self.price_cars_list, self.link_cars_list)
+            self.car_dict = dict(zip_iterator)
 
         def show_label():
-            zip_iterator = zip(self.price_cars_list, self.link_cars_list)
-            car_dict = dict(zip_iterator)
-            for prize, car in car_dict.items():
-                print(f"Prize - {prize} PLN/ Link - {car}")
+            for prize, car in self.car_dict.items():
+                print(f"Prize - {prize} PLN / Link - {car}")
 
-        sort_cars_list()
-        # sort_price_list()
+        create_label()
+        # price_ascending()
         show_label()
-
-        #TODO Napraw wyszukiwanie ceny i hrefu, zrob to w jednej funkcji aby sie to nie rozjezdżało
-        #TODO Sprobowac zrobic powyzszy punkt w Selenium
 
         #TODO Sortuj wyniki rosnaco, malejaco, wybierz dany przedział ceny
         #TODO Wyswietlaj ceny w wybranej walucie
