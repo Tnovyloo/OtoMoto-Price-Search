8f7473284fcd4d768504bcfc9146f2e46342b242
Tomasz <loobrome@gmail.com>
2021-12-27 15:20:24 +0100

    Changes in program operations

--- OtoMotoProgramClass.py
+++ OtoMotoProgramClass.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 class OtoMotoProgram:
     def __init__(self):
         load_dotenv()
-        self.URL = os.environ.get("URL")
+        # self.URL = os.environ.get("URL")
+        self.URL = input("Provide Otomoto link with \nsearch criteria and price criteria: ")
         self.response = requests.get(self.URL).text
         # self.soup = bs.BeautifulSoup(self.response, 'html5lib')
         self.soup = bs.BeautifulSoup(self.response, 'html.parser')
@@ -16,12 +17,12 @@ class OtoMotoProgram:
         self.page_list = []
         self.index_list = []
         self.car_dict = {}
+        self.choice = None
 
     def Start(self):
         # print(self.URL)
         self.DownloadPage()
         self.Work_with_data()
-
 
     def DownloadPage(self):
 
@@ -31,7 +32,6 @@ class OtoMotoProgram:
             cars_price = self.soup.findAll('span', class_='optimus-app-epvm6 e1b25f6f8')
 
             for price in cars_price:
-                # self.price_cars_list.append(price.text)
                 self.price_cars_list.append(str(price.text).strip('PLN'))
 
         def find_link():
@@ -74,44 +74,6 @@ class OtoMotoProgram:
 
     def Work_with_data(self):
 
-        # def sort_cars_list():
-        #     """Sorting elements in cars list, removing 'od' and 'price'
-        #     cause its advertising elements"""
-        #
-        #     # for element in self.price_cars_list:
-        #     #     if element == "od":
-        #     #         index = self.price_cars_list.index("od")
-        #     #         del self.price_cars_list[index:index + 2]
-        #     #
-        #     #     else:
-        #     #         continue
-        #     for element in self.price_cars_list:
-        #         if element == "od":
-        #             index = self.price_cars_list.index("od")
-        #             # del self.price_cars_list[index: index + 1]
-        #             del self.price_cars_list[index]
-        #
-        #         else:
-        #             continue
-        #
-        # def sort_price_list():
-        #     for element in self.index_list:
-        #         self.price_cars_list.pop(element)
-
-        #TODO Sortuj dictionary poprzez klucz, dokończ funkcję asceding()
-
-        def price_ascending():
-            # for key in self.car_dict.keys():
-            #     print(key)
-            # for key in sorted(self.car_dict.keys()):
-            #     print(key)
-            # self.car_dict = {k: v for k, v in sorted(list(self.car_dict.items()))}
-
-            for key in sorted(self.car_dict.keys()):
-                print(key)
-
-            #TODO zrobic sortowanie cen metodą dodania do linku '&search%5Border%5D=filter_float_price%3Aasc'
-
         def create_label():
             zip_iterator = zip(self.price_cars_list, self.link_cars_list)
             self.car_dict = dict(zip_iterator)
@@ -121,15 +83,6 @@ class OtoMotoProgram:
                 print(f"Prize - {prize} PLN / Link - {car}")
 
         create_label()
-        # price_ascending()
         show_label()
 
-        #TODO Sortuj wyniki rosnaco, malejaco, wybierz dany przedział ceny
         #TODO Wyswietlaj ceny w wybranej walucie
-
-
-        # print(self.price_cars_list)
-        # print(self.link_cars_list)
-        # print(len(self.link_cars_list), len(self.price_cars_list))
-        # # print(self.link_cars_list)
-        # print()
