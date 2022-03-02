8047cadb80a44300431e40d6d2ffa3599976c0dc
Tnovyloo <76777800+Tnovyloo@users.noreply.github.com>
2022-03-02 18:44:20 +0100

    Simplifier user interface in Fuel module, and adjustment code to that update

--- Program modules/Fuel_Data.py
+++ Program modules/Fuel_Data.py
@@ -5,11 +5,17 @@ table = pd.read_html(URL)
 df = table[0]
 
 def show_fuel_price():
-    print(df)
+    df_to_print = df['Unnamed: 0']
+    print(df_to_print.to_string())
 
 def find_price(province, multiplier, currency):
     # print(df[df['Unnamed: 0'].astype(str).str.contains(woj)])
-    province_price = df[df['Unnamed: 0'].astype(str).str.contains(province)].values[0]
+    # print(df['Unnamed: 0'])
+    user_province = df['Unnamed: 0'].iloc[int(province)]
+    province_price = df[df['Unnamed: 0'].astype(str).str.contains(user_province)].values[0]
+
+    if currency == 'PLN':
+        multiplier = 1
 
     fuel_95 = round(((float(province_price[1:][0]) * 0.01) * multiplier), 2)
     fuel_98 = round(((float(province_price[1:][1]) * 0.01) * multiplier), 2)
@@ -17,7 +23,6 @@ def find_price(province, multiplier, currency):
     fuel_on_plus = round(((float(province_price[1:][3]) * 0.01) * multiplier), 2)
     fuel_lpg = round(((float(province_price[1:][4]) * 0.01) * multiplier), 2)
 
-    #TODO CHANGE CURRENCY - nie dziala
     print(f"Average price for fuels in '{province.capitalize()}' province\n"
           f"95 - {fuel_95} {currency}/l\n"
           f"98 - {fuel_98} {currency}/l\n"
@@ -27,4 +32,4 @@ def find_price(province, multiplier, currency):
           )
 
 # show_fuel_price()
-# find_price(province= (input("Type province (for example 'małopolskie'): ").lower()))
+# find_price(province= (input("Type province: ")), multiplier=1, currency='PLN')
