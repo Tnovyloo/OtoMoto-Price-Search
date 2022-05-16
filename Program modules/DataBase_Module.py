ec8cef22145011e20d5a061e378938308d29c2c4
Tnovyloo <76777800+Tnovyloo@users.noreply.github.com>
2022-05-16 19:41:01 +0200

    Doing new module with creating tables in MySQL Database.

--- Program modules/DataBase_Module.py
+++ Program modules/DataBase_Module.py
@@ -3,14 +3,15 @@ import mysql.connector
 
 class SQL_Module:
     def __init__(self):
-        db_host = os.environ.get('DB_HOST')
-        db_user = os.environ.get('DB_USER')
-        db_password = os.environ.get('DB_PASSWORD')
-        db_name = os.environ.get('DB_NAME')
-        db = mysql.connector.connect(host=db_host,
-                                 user=db_user,
-                                 password=db_password,
-                                 database=db_name)
+        # db_host = os.environ.get('DB_HOST')
+        # db_user = os.environ.get('DB_USER')
+        # db_password = os.environ.get('DB_PASSWORD')
+        # db_name = os.environ.get('DB_NAME')
+        # db = mysql.connector.connect(host=db_host,
+        #                          user=db_user,
+        #                          password=db_password,
+        #                          database=db_name)
+        pass
 
     def checkTableExists(self, dbcon, tablename):
         dbcur = dbcon.cursor()
@@ -25,3 +26,11 @@ class SQL_Module:
 
         dbcur.close()
         return False
+
+    def extractDataFromURL(self, url:str):
+        temp_list = url.split('/')
+        brand = temp_list[4] #table name
+        print(brand)
+
+sql = SQL_Module()
+sql.extractDataFromURL('https://www.otomoto.pl/oferta/bmw-seria-5-piekne-rodzinne-bmw-520d-ID6Ei1VH.html')
