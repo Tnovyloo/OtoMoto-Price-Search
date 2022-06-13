3c9cd6e3d1692502eefe8088efafd34fd81c98e5
Tnovyloo <76777800+Tnovyloo@users.noreply.github.com>
2022-06-13 20:34:34 +0200

    Doing new module with creating tables in MySQL Database.

--- Program modules/DataBase_Module.py
+++ Program modules/DataBase_Module.py
@@ -1,36 +1,58 @@
-import os
+from os import environ
 import mysql.connector
 
 class SQL_Module:
-    def __init__(self):
-        # db_host = os.environ.get('DB_HOST')
-        # db_user = os.environ.get('DB_USER')
-        # db_password = os.environ.get('DB_PASSWORD')
-        # db_name = os.environ.get('DB_NAME')
-        # db = mysql.connector.connect(host=db_host,
-        #                          user=db_user,
-        #                          password=db_password,
-        #                          database=db_name)
-        pass
+    def __init__(self, url):
+        db_host = environ.get('DB_HOST')
+        db_user = environ.get('DB_USER')
+        db_password = environ.get('DB_PASSWORD')
+        print(db_user, db_password, db_host)
+        self.db = mysql.connector.connect(host=db_host,
+                                    user=db_user,
+                                    password=db_password,
+                                    )
+        self.url = url
+        # db = mysql.connector.connect(host="localhost",
+        #                              user="root",
+        #                              passwd="***REMOVED***",
+        #                              database="otomotoprogram")
 
-    def checkTableExists(self, dbcon, tablename):
-        dbcur = dbcon.cursor()
-        dbcur.execute("""
+        # mycursor = db.cursor()
+        # mycursor.execute("CREATE DATABASE otomotoprogram")
+
+    def create_table(self):
+        brand = self.extractDataFromURL(self.url)
+        if self.checkTableExists(brand) is False:
+            dbcursor = self.db.cursor()
+            dbcursor.execute(
+                f"""CREATE TABLE `otomotoprogram`.`{brand}` (
+                  `id` INT NOT NULL,
+                  `url` LONGTEXT NOT NULL,
+                  `price` VARCHAR(45) NOT NULL,
+                  PRIMARY KEY (`id`));""")
+        else:
+            print(f"there is a table like {brand}")
+
+    def checkTableExists(self, tablename):
+        dbcursor = self.db.cursor()
+        dbcursor.execute("""
             SELECT COUNT(*)
             FROM information_schema.tables
             WHERE table_name = '{0}'
             """.format(tablename.replace('\'', '\'\'')))
-        if dbcur.fetchone()[0] == 1:
-            dbcur.close()
+        if dbcursor.fetchone()[0] == 1:
+            dbcursor.close()
             return True
 
-        dbcur.close()
+        dbcursor.close()
         return False
 
     def extractDataFromURL(self, url:str):
         temp_list = url.split('/')
-        brand = temp_list[4] #table name
+        brand = temp_list[4].split('-')[0] #table name
         print(brand)
+        return brand
 
-sql = SQL_Module()
-sql.extractDataFromURL('https://www.otomoto.pl/oferta/bmw-seria-5-piekne-rodzinne-bmw-520d-ID6Ei1VH.html')
+url = 'https://www.otomoto.pl/oferta/bmw-seria-5-piekne-rodzinne-bmw-520d-ID6Ei1VH.html'
+sql = SQL_Module(url)
+sql.create_table()
