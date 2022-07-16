628b2903c1ee3b140ac3508cd3cc84888dc43c5a
Tnovyloo <76777800+Tnovyloo@users.noreply.github.com>
2022-07-16 12:18:02 +0200

    Checking TODO.

--- Program modules/Fuel_Data.py
+++ Program modules/Fuel_Data.py
@@ -21,23 +21,31 @@ def fuel_price_data(province, multiplier, currency):
     if currency == 'PLN':
         multiplier = 1
 
-    fuel_95 = round(((float(province_price[1:][0]) * 0.01) * multiplier), 2)
-    fuel_98 = round(((float(province_price[1:][1]) * 0.01) * multiplier), 2)
-    fuel_on = round(((float(province_price[1:][2]) * 0.01) * multiplier), 2)
-    fuel_on_plus = round(((float(province_price[1:][3]) * 0.01) * multiplier), 2)
-    fuel_lpg = round(((float(province_price[1:][4]) * 0.01) * multiplier), 2)
+    # fuel_95 = round(((float(province_price[1:][0]) * 0.01) * multiplier), 2)
+    # fuel_98 = round(((float(province_price[1:][1]) * 0.01) * multiplier), 2)
+    # fuel_on = round(((float(province_price[1:][2]) * 0.01) * multiplier), 2)
+    # fuel_on_plus = round(((float(province_price[1:][3]) * 0.01) * multiplier), 2)
+    # fuel_lpg = round(((float(province_price[1:][4]) * 0.01) * multiplier), 2)
 
-    # fuels = [round(((float(province_price[1:][i]) * 0.01) * multiplier), 2) for i in range(4)]
+
+    # print(f"Average price for fuels in '{province.capitalize()}' province\n"
+    #       f"95 - {fuel_95} {currency}/l\n"
+    #       f"98 - {fuel_98} {currency}/l\n"
+    #       f"ON - {fuel_on} {currency}/l\n"
+    #       f"ON+ - {fuel_on_plus} {currency}/l\n"
+    #       f"LPG - {fuel_lpg} {currency}/l"
+    #       )
+
+    fuels = [round(((float(province_price[1:][i]) * 0.01) * multiplier), 2) for i in range(5)]
     # print(fuels)
 
-    print(f"Average price for fuels in '{province.capitalize()}' province\n"
-          f"95 - {fuel_95} {currency}/l\n"
-          f"98 - {fuel_98} {currency}/l\n"
-          f"ON - {fuel_on} {currency}/l\n"
-          f"ON+ - {fuel_on_plus} {currency}/l\n"
-          f"LPG - {fuel_lpg} {currency}/l"
+    print(f"Average price for fuels in '{user_province.capitalize()}' province\n"
+          f"95 - {fuels[0]} {currency}/l\n"
+          f"98 - {fuels[1]} {currency}/l\n"
+          f"ON - {fuels[2]} {currency}/l\n"
+          f"ON+ - {fuels[3]} {currency}/l\n"
+          f"LPG - {fuels[4]} {currency}/l"
           )
-
 
 # if __name__ == "__main__":
 #    # stuff only to run when not called via 'import' here
